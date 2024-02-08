10b4121806385dd3ec88fd952d88f3e67c01a3d8
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-02-08 21:32:22 +0100

    Restructure generate.py to properly generate sections for the RightOperands
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- Profile.md
+++ Profile.md
@@ -4,8 +4,8 @@ Shortname: cx-profile
 Level: 1
 Status: w3c/UD
 Group: Data Sovereignty Task Force
-Repository: org/repo-name
-URL: http://example.com/url-this-spec-will-live-at
+Repository: https://github.com/catenax-eV/cx-odrl-profile
+URL: https://w3id.org/catenax/policy/
 Editor: Matthias Binzer
 Abstract: The Catena-X Profile for ODRL. This defines what is used in ODRL policies.
 Markup Shorthands: markdown yes, css no
@@ -16,8 +16,6 @@ Introduction {#intro}
 
 This is a Catena-X Profile for ODRL. It adds `leftOperand` instances and also limits what from ODRL can be used.
 
-leftOperand classes {#leftOperandClasses}
-======================================
 <pre class=include>
 path: ttl_generated_content.md
 </pre>
--- generate.py
+++ generate.py
@@ -8,14 +8,38 @@ import rdflib
 from string import Template
 
 TABLE_DEF ="""
+### $title
 <pre class='simpledef'>
+Identifier: <a href="$identifier">$identifier</a>
 Definition: $definition
 Label: $label
-Identifier: $identifier
 Note: $note
 Class: $myclass
+SubClassOf: <a href="$subclassOf">$subclassOf</a>
 </pre>
 """
+
+def query_prepare(query_for: str = 'odrl:LeftOperand'):
+    query_template = """
+    prefix : <https://w3id.org/catenax/policy/#>
+    prefix odrl: <http://www.w3.org/ns/odrl/2/>
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+    SELECT ?x ?label ?definition ?note ?scopeNote ?subclassOf ?xnoprefix
+    WHERE {
+        ?x a $query_for .
+        OPTIONAL { ?x rdfs:label ?label . }
+        OPTIONAL { ?x skos:definition ?definition . }
+        OPTIONAL { ?x skos:note ?note . }
+        OPTIONAL { ?x skos:scopeNote ?scopeNote . }
+        OPTIONAL { ?x rdfs:subClassOf ?subclassOf . }
+        bind(strafter(str(?x),str(:)) as ?xnoprefix)
+    }
+    """
+    query = Template(query_template).substitute(
+        query_for = query_for,
+    )
+    return query
 
 def generate(fn: str, out_fn: str):
 
@@ -26,33 +50,28 @@ def generate(fn: str, out_fn: str):
     g = rdflib.Graph()
     g.parse(data=data, format='turtle')
 
-    query = """
-    prefix : <http://www.catena-x.net/ns/policy/>
-    prefix odrl: <http://www.w3.org/ns/odrl/2/>
-    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-    SELECT ?x ?label ?definition ?note ?scopeNote
-    WHERE {
-        ?x a odrl:leftOperand .
-        ?x rdfs:label ?label .
-        ?x skos:definition ?definition .
-        ?x skos:note ?note .
-        ?x skos:scopeNote ?scopeNote
-    }
-    """
-
-    qres = g.query(query)
     data = ""
-    for row in qres:
-        print(f"{row.x} {row.label}")
-        item = Template(TABLE_DEF).substitute(
-            definition = row.definition,
-            label = row.label,
-            identifier = row.x,
-            note = row.note,
-            myclass = 'odrl.leftOperand',
-        )
-        data = data + item + "\n"
+
+    for q in [
+            'odrl:LeftOperand', 'odrl:RightOperand', ':usecaseFrameworkRightOperand', ':contractPurposeRightOperand']:
+        query = query_prepare(query_for=q)
+        qres = g.query(query)
+        data = data + f"{q} classes\n======================================"
+
+        for row in qres:
+            print(f"{row.x} {row.label}")
+            print(f"{row.xnoprefix}")
+            item = Template(TABLE_DEF).substitute(
+                title = row.xnoprefix,
+                definition = row.definition,
+                label = row.label,
+                identifier = row.x,
+                note = row.note,
+                myclass = q,
+                subclassOf = row.subclassOf or '',
+            )
+            data = data + item + "\n"
+
     with open(out_fn, 'wt') as f:
         f.write(data)
 
